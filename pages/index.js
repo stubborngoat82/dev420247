fdaae3e7c667bc1b18545659d83c62228a8ea798
Wes T <85148144+stubborngoat82@users.noreply.github.com>
2023-04-14 11:11:23 -0700

    updated

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,48 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <header>
+        <img src="https://ipfs.io/ipfs/QmVVigmjuftzivR2K7dQCuR9vaENzBrTgG8KAYCfHFueQh?filename=420247.png" class="logo " alt="420247 logo"> </img>
+      </header>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+      <div class="container">
+        <p>
+        Your place for WEED on WEB3
         </p>
-      </main>
+        <p>Join the weed culture revolution with a subdomain on 420247,
+          the top-level domain dedicated to everything weed-related! 
+           </p>
+        <p>
+          Stand out from your competition with a unique and memorable
+          web address that speaks directly to your audience.
+        </p>
+        <p>
+          Our affordable packages offer customization and support to suit your needs.
+          Do not miss out on this opportunity to make a lasting impression on the weed culture community.
+          Get your .420247 subdomain today. 
+      </p>
 
-      <Footer />
-    </div>
-  )
+      <p><h2>Domains start at $21.94 per year.</h2>  </p>
+    </div><div class="porkbun">
+        <a href="https://porkbun.com/tld/420247">Find your .420247 subdomain today! Starting at $21.</a>
+        <a href="https://porkbun.com/tld/420247"><img src="https://ipfs.io/ipfs/QmQMd9T1PF9YhuHA4n7S3GAassGurZEjHS2Q8W81xVpzta?filename=porkbun_rgb.png"></></a>
+      </div><nav>
+        <ul>
+          <li class="dropdown">
+            <a href="#" text-align="left">Reference Links</a>
+          </li>
+          <ul class="dropdown-menu">
+            <li><a href="https://learn.namebase.io">Namebase Learning Center</a></li>
+            <li><a href="https://www.namebase.io/use-cases/">Handshake Use Cases</a></li>
+            <li><a href="https://namercommunity.notion.site/Namer-Community-ca359569e7ce4a8597d0c52c78336815">Community</a></li>
+          </ul>
+
+        </ul>
+      </nav><footer>
+        <h6> Copyright 2023 StubbornGoat LLC</h6>
+      </footer>
+      </>
+
+  ) 
 }
